e905fcf8edd6e100fe6db496cbca0f1e6362ac79
SkeloGH <dev.aaron.gh@gmail.com>
2024-10-29 00:06:16 -0600

    expanded filter to be inclusive

--- filters.py
+++ filters.py
@@ -4,7 +4,7 @@ This module contains functions for applying filters to stock data.
 
 def fundamentals(ticker_data):
     """
-    Applies fundamental filters to a stock's data.
+    Inclusively filters tickers based on fundamental data.
 
     Parameters:
         ticker_data (dict): Dictionary containing financial data for the ticker.
@@ -15,9 +15,9 @@ def fundamentals(ticker_data):
     if None in ticker_data.values():
         return False
     return (
-        10 <= ticker_data['PE_ratio'] <= 25 and
-        ticker_data['PB_ratio'] < 3 and
-        ticker_data['ROE'] > 10 and
-        ticker_data['Current_Ratio'] > 1.5 and
+        10 <= ticker_data['PE_ratio'] <= 25 or
+        ticker_data['PB_ratio'] < 3 or
+        ticker_data['ROE'] > 10 or
+        ticker_data['Current_Ratio'] > 1.5 or
         ticker_data['Debt_Equity'] < 0.5
     )
