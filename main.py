c6721457e32aa71e0fdbffc92f161c8ab1d79280
SkeloGH <dev.aaron.gh@gmail.com>
2024-10-28 22:10:04 -0600

    code cleanup

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import logging
 
 from reporting import reset_decision_log, output_summary
-from tickers import get_tickers_list_by_index, prompt_ticker_selection, prompt_custom_ticker_list
+from tickers import get_tickers_list_by_index
+from user_prompts import prompt_ticker_selection, prompt_custom_ticker_list
 from ticker_data import fetch_tickers, get_tickers_fundamentals, is_market_open
 from classify import identify_bullish_bearish
 from config import CONFIG
---
+++ user_prompts.py
@@ -0,0 +1,34 @@
+"""Prompts for user input."""
+
+from config import CONFIG
+
+def prompt_ticker_selection():
+    """Prompt the user to choose a list of tickers."""
+    while True:
+        print("Choose a list of tickers:")
+
+        for i, ticker_list in enumerate(CONFIG['TICKERS_LISTS']):
+            print(f"{i+1}. {ticker_list['name']} ({ticker_list['description']})")
+
+        choice = input("Enter your choice: ")
+
+        if choice.isdigit() and 1 <= int(choice) <= len(CONFIG['TICKERS_LISTS']):
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    return choice
+
+def prompt_custom_ticker_list():
+    """Prompt the user to enter a list of tickers."""
+    while True:
+        custom_tickers = input("Enter a list of tickers separated by commas: ").strip().split(',')
+        # Sanitize input
+        custom_tickers = [ticker.strip() for ticker in custom_tickers if ticker.strip()]
+
+        if not custom_tickers:
+            print("Invalid input. Please try again.")
+        else:
+            break
+
+    return custom_tickers
