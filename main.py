c219e2f80d7d189260dc6374b2d132d6acc14cde
SkeloGH <dev.aaron.gh@gmail.com>
2024-11-06 12:23:38 -0600

    code cleanup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from config import CONFIG
 from user_inputs import handle_keyboard_interrupt, prompt_ticker_selection, prompt_custom_ticker_list
 from tickers import get_tickers_list_by_index
-from ticker_data import fetch_tickers_by_fundamentals, get_tickers_historical_data, fetch_tickers, fetch_tickers_prices, is_market_open
+from ticker_data import fetch_tickers_by_fundamentals
 from classify import classify_tickers
 from indicators import generate_targets
 from reporting import reset_decision_log, output_summary, format_summary
@@ -40,14 +40,12 @@ def main():
 
     # Identify the top movers
     bullish_tickers, bearish_tickers = classify_tickers(compliant_tickers_data)
-    # Fetch prices
-    # prices = fetch_tickers_prices(compliant_ticker_names)
     # Generate buy and sell targets
     buy_targets, sell_targets = generate_targets(bullish_tickers, bearish_tickers)
-    formatted_summary = format_summary(compliant_tickers_data, buy_targets, sell_targets)
+    trading_signals = format_summary(compliant_tickers_data, buy_targets, sell_targets)
 
     # Generate and display summary
-    output_summary(formatted_summary)
+    output_summary(trading_signals)
 
 if __name__ == "__main__":
     main()
